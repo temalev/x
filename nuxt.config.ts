0adf5d84ab0f87f5c9e7ac89edfc3e82cb084533
Artem <myartoym@gmail.com>
2025-11-21 15:31:04 +0300

    fix title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,9 +8,9 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css', '~/assets/css/common.scss'],
   app: {
     head: {
-      title: 'xnogi',
+      title: 'ИксНоги — авторские игрушки и оригинальная вселенная персонажей',
       meta: [
-        { name: 'description', content: 'Сайт икс ног' }
+        { name: 'description', content: 'ИксНоги — авторские игрушки и уникальные персонажи со своей вселенной, мультфильмами и коллекционными фигурками независимого цифрового художника.' }
       ]
     }
   }
